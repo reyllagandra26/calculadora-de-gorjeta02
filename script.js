fe238535b47c7dbac19910a4606c51a8e52f8c97
Reylla Gandra <reyllagandra26@gmail.com>
2026-06-01 02:36:24 -0300

    nono do css e terceiro do js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
@@ -25,4 +26,21 @@ function receberQuantidadePessoas(evento) {
   }
 }
 
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+  botoesGorjeta.forEach(botao => {
+  botao.addEventListener("click", receberPorcentagem)
+})
+
+function receberPorcentagem(evento) {
+   botoesGorjeta.forEach(botao => {
+    botao.classList.remove("botao-ativo")
+
+  if(botao.value === evento.target.value) {
+    botao.classList.add("botao-ativo")
+  }
+})
+
+  porcentagem = parseFloat(evento.target.value) / 100
+
+}  
 
